f6c5f727fc73dde8b06f216f0760e14a29963a51
sathwikraja <sathwiktraja@gmail.com>
2026-02-22 20:27:22 -0600

    Create DataWarehouse database and schemas
    Initialize the database and create schemas for data warehousing.

---
+++ scripts/init_database.sql
@@ -0,0 +1,14 @@
+USE master;
+
+#creating database
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+#creating schemas
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
